6f152031f80b4a565b8ffd87a3e2ffa5a52cd1bd
TomAgacci <dementedmax@gmail.com>
2026-06-08 23:23:47 -0700

    Create palettes.c

---
+++ colorfoldermaster/firmware/color/palettes.c
@@ -0,0 +1,22 @@
+#include "palettes.h"
+
+const uint8_t palette_r[NTSC_NBANKS][256] PROGMEM = {
+    { /* bank 0 R[0..255] */ },
+    { /* bank 1 */ },
+    { /* bank 2 */ },
+    { /* bank 3 */ }
+};
+
+const uint8_t palette_g[NTSC_NBANKS][256] PROGMEM = {
+    { /* bank 0 G */ },
+    { /* bank 1 */ },
+    { /* bank 2 */ },
+    { /* bank 3 */ }
+};
+
+const uint8_t palette_b[NTSC_NBANKS][256] PROGMEM = {
+    { /* bank 0 B */ },
+    { /* bank 1 */ },
+    { /* bank 2 */ },
+    { /* bank 3 */ }
+};
