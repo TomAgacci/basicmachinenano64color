bb437cac206c4b87056b3962ef7a42d6da8b0070
TomAgacci <dementedmax@gmail.com>
2026-07-01 16:52:46 -0700

    Add CompositeColor class for color mapping

---
+++ minecraft/firmware/nano64/src/composite_colorv2.cpp
@@ -0,0 +1,11 @@
+#include "composite_color.h"
+#include <vector>
+
+CompositeColor::CompositeColor(int w, int h)
+    : width(w), height(h), colors(w * h, 0) {}
+
+void CompositeColor::mapMonoToColor(const std::vector<bool> &monoFb) {
+    for (int i = 0; i < width * height && i < (int)monoFb.size(); ++i) {
+        colors[i] = monoFb[i] ? 7 : 0; // on=gray, off=black
+    }
+}
